4ae4839a1c83be1b05dac692f95b8f1bd9570ad4
thainta <thai1612200110a@gmail.com>
2023-08-09 10:34:13 +0700

    gift exchange feature

--- Server/index.js
+++ Server/index.js
@@ -1,16 +1,17 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import dotenv from 'dotenv'
-import cors from 'cors';
+import dotenv from "dotenv";
+import cors from "cors";
 
 import userRoute from "./routes/userRoute.js";
 import authRoute from "./routes/authRoute.js";
 import productRoute from "./routes/productRoute.js";
-import bannerRoute from "./routes/bannerRoute.js"
+import bannerRoute from "./routes/bannerRoute.js";
+import giftRoute from "./routes/giftRoute.js";
 const app = express();
 
-dotenv.config()
+dotenv.config();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
@@ -27,6 +28,7 @@ app.use("/", authRoute);
 app.use("/api/user", userRoute);
 app.use("/api/product", productRoute);
 app.use("/api/banner", bannerRoute);
+app.use("/api/gift", giftRoute);
 
 app.listen(port, () => {
   console.log(`Server listening on the port  ${port}`);
